d344f72cabe3993eefd851653ca9d059934d2f20
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-23 14:44:56 -0700

    Added methods to class Hogawarts::Studies

---
+++ hogwarts-cli-app/lib/hogwarts/studies.rb
@@ -0,0 +1,11 @@
+class Hogwarts::Studies
+    attr_accessor :name, :url, :bio, :sorting_hat_poem
+    def initialize(name, url)
+        @name = name
+        @url = url
+
+def call
+    puts list
+    choice
+end
+
