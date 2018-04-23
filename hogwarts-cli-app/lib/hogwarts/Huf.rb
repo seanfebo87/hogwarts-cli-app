6cc54e6df8a0b0da96490edffb8572aef5d245c2
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-23 12:30:54 -0700

    Added new classes.

---
+++ hogwarts-cli-app/lib/hogwarts/Huf.rb
@@ -0,0 +1,7 @@
+class Huf
+    attr_accessor :bio, :sorting_hat_poem
+
+def initialize
+    url = "https://www.pottermore.com/collection/all-about-hufflepuff"
+end
+end
---
+++ hogwarts-cli-app/lib/hogwarts/Sly.rb
@@ -0,0 +1,7 @@
+class Sly
+    attr_accessor :bio, :sorting_hat_poem
+
+def initialize
+    url = "https://www.pottermore.com/collection/all-about-slytherin"
+end
+end
