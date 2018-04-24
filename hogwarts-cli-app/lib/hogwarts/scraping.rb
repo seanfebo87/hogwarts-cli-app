95d2cbf580647410ff9bb79f200edd6f961571e8
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-24 14:40:33 -0700

    ,

--- hogwarts-cli-app/lib/hogwarts/scraping.rb
+++ hogwarts-cli-app/lib/hogwarts/scraping.rb
@@ -8,11 +8,10 @@ class Scraping
 def self.book_list(url)
     books = []
     doc = Nokogiri::HTML(open(url))
-    lists = doc.css(".col-md-12 ul li a")
-    lists.each do |n|
+    info = doc.css(".col-md-12 ul li a")
+    info.each do |n|
         list = {}
         list[:name] = n.text
-        list[:url] = n.attr("href")
         books << list
     end
     books
