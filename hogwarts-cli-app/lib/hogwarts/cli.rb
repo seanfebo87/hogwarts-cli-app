10e52e6512c4af0cfcde71f7fe7bbc10ca01d2b2
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-24 13:50:34 -0700

    Changes.

--- hogwarts-cli-app/lib/hogwarts/cli.rb
+++ hogwarts-cli-app/lib/hogwarts/cli.rb
@@ -1,17 +1,32 @@
-class Cli
+class Hogwarts::CLI
 
 def start
-    puts "Welcome to Hogwarts!
-         Please choose a magical book to learn more about..."
+    puts "Welcome to Hogwarts!"
+    puts "Please choose a magical book to learn more about..."
 
-    books = create_books
     books
+    menu
 end
 
 
-def create_books
-    new = Scraping.book_list("www.hp-lexicon.org/list/books/textbooks/")
-    new.map {|x| Hogwarts::Studies.new(x[:name], x[:url])}
+def books
+    @books = Hogwarts::Studies.list
+    @books.each.with_index(1) do |book, i|
+      puts "#{i}. #{book}"
+    end
 end
 
+def menu
+  input = nil
+  while input != "exit"
+  input = gets.strip
+  if input.to_i > 0
+    puts @books[input.to_i-1]
+  elseif input == "list"
+    books
+  else
+    puts "Having trouble?"
+  end
+end
+end
 end
--- hogwarts-cli-app/lib/hogwarts/studies.rb
+++ hogwarts-cli-app/lib/hogwarts/studies.rb
@@ -1,7 +1,15 @@
 class Hogwarts::Studies
-    attr_accessor :name, :url
-    def initialize(name, url)
-        @name = name
-        @url = url
-    end
+    attr_accessor :name, :detail
+
+
+
+    def self.list
+      book_1 = self.new
+      book_1.name = "Book 1"
+      book_1.detail = "Details"
+      book_2 = self.new
+      book_2.name = "Book 1"
+      book_2.detail = "Details"
+      [book_1, book_2]
+end
 end
