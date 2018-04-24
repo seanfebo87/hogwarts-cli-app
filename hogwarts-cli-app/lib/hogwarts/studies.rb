5a669332a99432f9210f3feff91321b37fb2035f
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-24 10:38:59 -0700

    Change for testing.

--- hogwarts-cli-app/lib/hogwarts/studies.rb
+++ hogwarts-cli-app/lib/hogwarts/studies.rb
@@ -1,14 +1,12 @@
 class Hogwarts::Studies
     attr_accessor :name, :url, :bio
-    def initialize(name, url)
-        @name = name
-        @url = url
+
 
 def call
     list
-    choice
 end
 
 def list
-    self.subject_list("http://wwo.wikidot.com/subject")
+    Scraping.subject_list("http://wwo.wikidot.com/subject")
+end
 end
