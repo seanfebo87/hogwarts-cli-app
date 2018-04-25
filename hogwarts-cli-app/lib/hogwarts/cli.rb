c12482ddd2a61ead6aace88c19babcf2d0bf9a9c
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-04-25 11:56:24 -0700

    change

--- hogwarts-cli-app/lib/hogwarts/cli.rb
+++ hogwarts-cli-app/lib/hogwarts/cli.rb
@@ -2,31 +2,84 @@ class Hogwarts::CLI
 
 def start
     puts "Welcome to Hogwarts!"
-    puts "Please choose a magical book to learn more about..."
-
-    books
+    puts "Please choose a magical category to learn more about..."
+    puts ""
+    puts "1. TextBooks"
+    puts "2. Atlas of Hogwarts"
     menu
 end
 
 
-def books
-    @books = Hogwarts::Studies.list
-    @books.each.with_index(1) do |book, i|
-      puts "#{i}. #{book}"
+def menu
+    books = make_books
+    atlas = make_atlas
+    input = gets.strip
+    if input.to_i == 1
+        list_books(books)
+    elsif input.to_i == 2
+        list_atlas(atlas)
+    else
+      "Please try again"
     end
 end
 
-def menu
-  input = nil
-  while input != "exit"
-  input = gets.strip
-  if input.to_i > 0
-    puts @books[input.to_i-1]
-  elseif input == "list"
-    books
-  else
-    puts "Having trouble?"
+def make_books
+    books_array = Hogwarts::Studies.scrape_books
+    books_array.map do |book|
+      Hogwarts::Studies.new(book[:name], book[:url])
+    end
+end
+
+def make_atlas
+    atlas_array = Hogwarts::Atlas.scrape_atlas
+    atlas_array.map do |n|
+      Hogwarts::Atlas.new(n[:name], n[:url])
+    end
+end
+
+
+def list_books(books)
+    puts ""
+    books.each.with_index(1) do |title, i|
+      puts "#{i}. #{title.name}"
+    end
+    puts ""
+    puts "Please select a book for link."
+    menu_book
   end
+
+
+
+
+
+def menu_book
+    input = gets.strip
+    if input.to_i > 0 && input.to_i <= 35
+      link = Hogwarts::Studies.all[input.to_i-1]
+      puts "#{link.url}"
+    else
+        "Please try again."
+    end
 end
+
+def list_atlas(atlas)
+     puts ""
+    atlas.each.with_index(1) do |title, i|
+      puts "#{i}. #{title.name}"
+    end
+    puts ""
+    puts "Please select a book for link."
+    menu_atlas
+  end
+
+def menu_atlas
+    input = gets.strip
+    if input.to_i > 0 && input.to_i <= 19
+      link = Hogwarts::Atlas.all[input.to_i-1]
+      puts "#{link.url}"
+    else
+        "Please try again."
+    end
 end
+
 end
--- hogwarts-cli-app/lib/hogwarts/studies.rb
+++ hogwarts-cli-app/lib/hogwarts/studies.rb
@@ -1,15 +1,46 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Hogwarts::Studies
-    attr_accessor :name, :detail
+    attr_accessor :name, :url, :bio
+    @@all = []
 
 
 
-    def self.list
-      book_1 = self.new
-      book_1.name = "Book 1"
-      book_1.detail = "Details"
-      book_2 = self.new
-      book_2.name = "Book 1"
-      book_2.detail = "Details"
-      [book_1, book_2]
-end
+    def initialize(name, url)
+     @name = name
+     @url = url
+     @@all << self
+    end
+
+   def self.scrape_books
+        books = []
+        doc = Nokogiri::HTML(open("https://www.hp-lexicon.org/list/books/textbooks/"))
+        info = doc.css(".col-md-12 ul li a")
+        info.each do |n|
+            book = {}
+            book[:name] = n.text
+            book[:url] = n.attr("href")
+            books << book
+
+        end
+        books
+    end
+
+
+
+
+
+
+    def self.scrape_info(url)
+        doc = Nokogiri::HTML(open(url))
+        info = doc.css("col-md-8 p").text
+        info
+    end
+
+    def self.all
+        @@all
+    end
+
+
 end
